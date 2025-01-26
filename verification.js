f33a877fa576118bf372f3eac523a3180998aa9e
vaibhavj27 <vj9766876963@gmail.com>
2025-01-27 00:56:45 +0530

    Verified

---
+++ verification.js
@@ -0,0 +1,17 @@
+
+
+
+var text= document.getElementById("verified");
+    var button=document.getElementById("button");
+    var code=document.getElementById("inputbox");
+    button.addEventListener("click",()=>{
+        if(code.value=="CA/JA1/1811")
+            {
+              text.innerHTML="Your Student I'd is Verified";
+            }else if(code.value=""){
+                alert("Please enter a code");
+            }else{
+                text.innerHTML="Your Student I'd is not Verified";
+            }
+    });
+    
